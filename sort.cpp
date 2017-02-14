0970f98f4f9149f468fcbb347c64637f9dd979c3
MasterT1 <andre.brathwaite5@mycavehill.uwi.edu>
2017-02-14 11:50:47 -0400

    Added sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstdlib>//for "exit()" on some systems
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+\fn linearSearch
+\brief Search data for the first occurrence of key
+\param [in] key The value being searched for
+\param [in] data The data set that will be searched
+\returns location of key if found or -1 if not found
+*/
+//int linearSearch(auto data, auto key);//prototype
+void selectionSort(auto &Data);
+void printVector(auto &inputs);
+
+
+int main()
+{
+  vector<string> inputs;
+  string search_key, input;   
+
+   cout<<"Welcome to \"Sort it\". We first need some input data."<<endl;
+   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+
+   cin>>input;
+ 
+    while(input != "#")//read an unknown number of inputs from keyboard
+    {
+       inputs.push_back(input);
+       cin>>input;
+    }
+
+   cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
+
+  if(inputs.size() == 0)//no input
+  {
+      cout<<endl<<"No input received, quiting..."<<endl<<endl;
+       exit(1);//nothing to do but quit program
+  }   
+   
+   selectionSort(inputs);
+   
+   printVector(inputs);     
+
+   return 0;
+}
+
+
+
+void selectionSort(auto & Data)
+{
+	for (int i= 0; i < Data.size(); i++)
+	{
+		int min = i;
+		for (int j = (i+1); j < Data.size(); j++)
+		{
+			if  (Data[j] < Data[min])
+			{
+				min = j; //Updates min index
+			}
+			swap(Data[i],Data[min]);			
+		}
+	}
+}
+
+void printVector(auto &newprintInput)
+{
+	unsigned int vectorSize = newprintInput.size();
+	
+	cout << "Results: ";
+	
+	for (unsigned int i = 0; i < vectorSize; i++)
+	{
+		cout << newprintInput[i]<< ", ";		
+	}
+	
+	cout << endl;
+
+}
